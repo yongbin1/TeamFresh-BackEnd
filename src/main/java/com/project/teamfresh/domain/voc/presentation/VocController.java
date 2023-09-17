6f34a8a95922ddc2ebd17d71e59b9deecbc7d48c
yongbin1 <rhdydqls1024@naver.com>
2023-09-17 17:04:23 +0900

    :sparkles: :: CreateVOC, GetVOCList API 개발

--- src/main/java/com/project/teamfresh/domain/voc/presentation/VocController.java
+++ src/main/java/com/project/teamfresh/domain/voc/presentation/VocController.java
@@ -1,11 +1,37 @@
 package com.project.teamfresh.domain.voc.presentation;
 
+import com.project.teamfresh.domain.voc.presentation.dto.request.CreateVocRequest;
+import com.project.teamfresh.domain.voc.presentation.dto.response.VocListResponse;
+import com.project.teamfresh.domain.voc.service.CreateVocService;
+import com.project.teamfresh.domain.voc.service.GetVocListService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/voc")
 public class VocController {
+
+    private final CreateVocService createVocService;
+    private final GetVocListService getVocListService;
+
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createVoc(
+            @RequestBody CreateVocRequest request
+    ) {
+        createVocService.execute(request);
+    }
+
+    @GetMapping("/list")
+    public VocListResponse getVocList() {
+        return getVocListService.execute();
+    }
+
 }
