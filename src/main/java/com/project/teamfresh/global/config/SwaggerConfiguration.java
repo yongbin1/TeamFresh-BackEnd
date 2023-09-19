5694b24bf85bfd04192c7923f15d360671997f42
yongbin1 <rhdydqls1024@naver.com>
2023-09-19 12:07:25 +0900

    :memo: :: Swagger API 문서 의존성 추가 및 코드 작성

--- src/main/java/com/project/teamfresh/global/config/SwaggerConfiguration.java
+++ src/main/java/com/project/teamfresh/global/config/SwaggerConfiguration.java
@@ -1,2 +1,24 @@
-package com.project.teamfresh.global.config;public class SwaggerConfiguration {
+package com.project.teamfresh.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("[팀프레시] 시스템개발3팀 과제 API")
+                .version("version 1.0")
+                .description("[팀프레시] 시스템개발3팀 과제 API 문서");
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
+
 }
--- src/main/java/com/project/teamfresh/global/security/SecurityConfiguration.java
+++ src/main/java/com/project/teamfresh/global/security/SecurityConfiguration.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsUtils;
@@ -27,22 +29,39 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(HttpBasicConfigurer::disable)
+                .csrf(CsrfConfigurer::disable)
+                .formLogin(FormLoginConfigurer::disable)
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                        .requestMatchers("/swagger-ui/**", "/configuration/**", "/swagger-resources/**", "/v3/api-docs/**", "/webjars/**",
+                                "/webjars/springfox-swagger-ui/*.{js,css}", "/bus/v3/api-docs/**").permitAll()
 
                         // Carrier 서버
                         .requestMatchers(HttpMethod.POST, "/carrier/**").permitAll() // 운송사 등록 API
+
+                        // Driver 서버
                         .requestMatchers(HttpMethod.POST, "/driver/**").permitAll() // 기사 등록 API
 
                         // Customer 서버
                         .requestMatchers(HttpMethod.POST, "/customer/**").permitAll() // 고객사 등록 API
+
+                        // Keeper 서버
                         .requestMatchers(HttpMethod.POST, "/keeper/**").permitAll() // 담당자 등록 API
+
+                        // VOC 서버
+                        .requestMatchers(HttpMethod.POST, "/voc/**").permitAll() // VOC 등록 API
+                        .requestMatchers(HttpMethod.GET, "/voc/**").permitAll() // VOC 목록 API
+
+                        // Compensation 서버
+                        .requestMatchers(HttpMethod.POST, "/compensation/**").permitAll() // 배상정보 등록 API
+                        .requestMatchers(HttpMethod.GET, "/compensation/**").permitAll() // 배상정보 목록 API
+
+                        // Penalty 서버
+                        .requestMatchers(HttpMethod.POST, "/penalty/**").permitAll() // 패널티 등록 API
 
                         .anyRequest().denyAll()
                 );
