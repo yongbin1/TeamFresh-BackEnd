6375f7fae2b86df0328c06e37d51ded54616d704
yongbin1 <rhdydqls1024@naver.com>
2023-09-19 21:44:46 +0900

    :memo: :: Swagger 문서 작성 및 LogFilter 생성

--- src/main/java/com/project/teamfresh/global/security/SecurityConfiguration.java
+++ src/main/java/com/project/teamfresh/global/security/SecurityConfiguration.java
@@ -4,14 +4,9 @@ import com.project.teamfresh.global.security.logger.LogFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
-import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -26,46 +21,21 @@ public class SecurityConfiguration {
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web ->
                 web.ignoring()
-                        .requestMatchers("/swagger-ui/**", "/configuration/**", "/swagger-resources/**", "/v3/api-docs/**", "/webjars/**", "/webjars/springfox-swagger-ui/*.{js,css}", "/bus/v3/api-docs/**");
+                        .mvcMatchers("/swagger-ui/**", "/configuration/**", "/swagger-resources/**", "/v3/api-docs/**", "/webjars/**", "/webjars/springfox-swagger-ui/*.{js,css}", "/bus/v3/api-docs/**");
     }
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
-                .httpBasic(HttpBasicConfigurer::disable)
-                .csrf(CsrfConfigurer::disable)
-                .formLogin(FormLoginConfigurer::disable)
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .authorizeHttpRequests(requests -> requests
-                        .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-
-                        // Carrier 서버
-                        .requestMatchers(HttpMethod.POST, "/carrier/**").permitAll() // 운송사 등록 API
-
-                        // Driver 서버
-                        .requestMatchers(HttpMethod.POST, "/driver/**").permitAll() // 기사 등록 API
-
-                        // Customer 서버
-                        .requestMatchers(HttpMethod.POST, "/customer/**").permitAll() // 고객사 등록 API
-
-                        // Keeper 서버
-                        .requestMatchers(HttpMethod.POST, "/keeper/**").permitAll() // 담당자 등록 API
-
-                        // VOC 서버
-                        .requestMatchers(HttpMethod.POST, "/voc/**").permitAll() // VOC 등록 API
-                        .requestMatchers(HttpMethod.GET, "/voc/**").permitAll() // VOC 목록 API
-
-                        // Compensation 서버
-                        .requestMatchers(HttpMethod.POST, "/compensation/**").permitAll() // 배상정보 등록 API
-                        .requestMatchers(HttpMethod.GET, "/compensation/**").permitAll() // 배상정보 목록 API
-
-                        // Penalty 서버
-                        .requestMatchers(HttpMethod.POST, "/penalty/**").permitAll() // 패널티 등록 API
-
-                        .anyRequest().denyAll()
-                )
+                .httpBasic().disable()
+                .csrf().disable()
+                .formLogin().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                .anyRequest().permitAll()
+                .and()
                 .addFilterBefore(new LogFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
--- src/main/java/com/project/teamfresh/global/security/logger/LogFilter.java
+++ src/main/java/com/project/teamfresh/global/security/logger/LogFilter.java
@@ -1,12 +1,12 @@
 package com.project.teamfresh.global.security.logger;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collections;
 
